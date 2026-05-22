bb3b865d0edb223e22f991ac35aeec5211555c59
Swati Basliyal <swatibasliyal24@gmail.com>
2026-05-22 11:27:47 +0530

    added new code

--- hello.py
+++ hello.py
@@ -3,3 +3,13 @@ old = False
 a=None
 print(type(old))
 print(type(a))
+a=1000
+b=500
+sum=a+b
+print(sum)
+difference=a-b
+print(difference)
+product=a*b
+print(product)
+quotient=a/b
+print(quotient)
