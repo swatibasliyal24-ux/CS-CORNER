5f69be79e1d510fb43c5e99f111ff0c691506356
Swati Basliyal <swatibasliyal24@gmail.com>
2026-05-22 11:40:19 +0530

    added new code

--- hello.py
+++ hello.py
@@ -3,13 +3,19 @@ old = False
 a=None
 print(type(old))
 print(type(a))
+#arithmetic operators
 a=1000
 b=500
 sum=a+b
 print(sum)
-difference=a-b
-print(difference)
+print(a-b)
 product=a*b
 print(product)
-quotient=a/b
+quotient=a/b #gives the quotient with the decimal part
 print(quotient)
+print(a%b)
+print(a**b)
+print(a//b) #gives the quotient without the decimal part
+#Hi,I am learning Python programming language.
+"""multi-line
+comment"""
